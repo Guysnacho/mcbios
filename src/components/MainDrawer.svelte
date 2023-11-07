40cdaf460759bac7da8f12bb2a91c69a99c29a0c
Guysnacho <guysnacho2@gmail.com>
2023-11-06 22:18:21 -0600

    Add navbar and drawer

---
+++ src/components/MainDrawer.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	const drawerStore = getDrawerStore();
+</script>
+
+<Drawer>
+	{#if $drawerStore.id === 'nav'}
+		<!-- (show 'example-1' contents) -->
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Home
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Abstract Submission
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Registration
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Accomodations
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Program
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			About Us
+		</a>
+		<a class="flex btn rounded variant-soft-surface overflow-hidden" href="/" rel="noreferrer">
+			Young Scientist Excellence Awards
+		</a>
+	{/if}
+</Drawer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, type DrawerSettings, getDrawerStore } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	import MainDrawer from '../components/MainDrawer.svelte';
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
+	let sidebarVisible = false;
+	const drawerSettings: DrawerSettings = {
+		id: 'nav',
+		position: 'right',
+		width: '20%'
+	};
 </script>
+
+<MainDrawer />
 
 <!-- App Shell -->
 <AppShell>
@@ -12,15 +26,69 @@
 				<a href="/"><strong class="text-xl uppercase ml-4">MCBIOS</strong></a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer"> Home </a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer">
+				<div class="flex items-center lg:hidden">
+					<button
+						class="btn btn-sm mr-4"
+						on:click={() => {
+							drawerStore.open(drawerSettings);
+						}}
+					>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+				</div>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
+					Home
+				</a>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
 					Abstract Submission
 				</a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer"> Registration </a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer"> Accomodations </a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer"> Program </a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer"> About Us </a>
-				<a class="btn btn-sm variant-soft-surface" href="/" rel="noreferrer">
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
+					Registration
+				</a>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
+					Accomodations
+				</a>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
+					Program
+				</a>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
+					About Us
+				</a>
+				<a
+					class="hidden lg:flex lg:max-xl:visible btn btn-sm variant-soft-surface overflow-hidden"
+					href="/"
+					rel="noreferrer"
+				>
 					Young Scientist Excellence Awards
 				</a>
 			</svelte:fragment>
