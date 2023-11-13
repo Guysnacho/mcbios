5291b8b9492412a6dc8fb8e9b90fcf850ab31737
guysnacho <guysnacho2@gmail.com>
2023-11-12 21:37:40 -0600

    Start migration to flex because I hate grid rows and templates

--- src/routes/abstract/+page.svelte
+++ src/routes/abstract/+page.svelte
@@ -29,11 +29,13 @@
 		</div>
 	</div>
 </div>
-
-<div class="grid lg:grid-cols-2 grid-cols-1 space-y-10">
-	<div class="space-y-3 w-5/6 sm:w-4/5 md:w-2/3 lg:w-3/5 mx-auto col-span-1">
-		<h4 class="h4 my-7 mx-2 text-center">2023 MCBIOS Abstract Submission Instructions</h4>
-		<p>Please use the EasyChair Portal to submit an abstract and indicate the following:</p>
+<!-- Flex box 1 -->
+<div class="flex flex-col md:flex-row">
+	<div class="mx-auto basis-1/2 w-3/4 md:w-6/12 md:px-10">
+		<h4 class="h4 my-7 mx-2 text-center">2024 MCBIOS Abstract Submission Instructions</h4>
+		<p class="-mx-4">
+			Please use the EasyChair Portal to submit an abstract and indicate the following:
+		</p>
 		<ul class="list-disc space-y-2">
 			<li>Indicate presentation type (Oral or Poster)</li>
 			<li>
@@ -46,16 +48,51 @@
 			</li>
 		</ul>
 	</div>
-	<div class="space-y-5 mx-auto px-10 col-span-1">
-		<p class="h4 my-7 mx-2 text-center font-thin">YSEA Instructions</p>
-		<p class="w-5/6 m-auto">
+	<div class="py-auto basis-1/2 w-3/4 mx-auto md:w-6/12 md:px-10">
+		<p class="h3 my-7 mx-2 text-center">YSEA Instructions</p>
+		<p class="m-auto">
 			Indicate if competing for the “MCBIOS Young Scientist Excellence Award” (only applies to
 			student and post-doc categories). If this option is selected, we will provide instructions for
 			submission of materials for the award application.
-			<br />
-			<br />
-			For the award application, you will provide additional information for evaluation: Innovation in
-			research, author's contribution to the research, and statement of academic integrity.
+		</p>
+		<p class="m-auto">
+			For the award application, you will provide additional information for evaluation: Innovation
+			in research, author's contribution to the research, and statement of academic integrity.
+		</p>
+	</div>
+</div>
+
+<hr class="!border-t-4 !border-dotted my-5" />
+<!-- Flex box 2 -->
+<div class="flex flex-col md:flex-row">
+	<div class="mx-auto basis-1/2 w-3/4 md:w-6/12 md:px-10">
+		<p class="-mx-4 font-semibold">
+			The Abstract should not exceed 300 words. Please minimize the use of abbreviations and do not
+			cite references in the abstract.
+		</p>
+		<p class="-mx-4">The abstract must include the following separate sections:</p>
+		<ul class="list-disc space-y-2">
+			<li>Background: Context and purpose of the study</li>
+			<li>Results: Main findings</li>
+			<li>Conclusions: A brief summary and potential implications</li>
+		</ul>
+	</div>
+	<div class="space-y-5 mx-auto px-10 col-span-1">
+		<h6 class="text-center">Oral Presentations</h6>
+		<p class="w-5/6 m-auto">
+			There are a limited number of spots for oral presentations. If not selected for oral
+			presentation, you will automatically be considered for poster presentation. Oral presentations
+			will be 10-15 min long including questions, depending on the session program designed by the
+			session chair. Poster size limitation: <span class="font-semibold"
+				>30"W x 20"H (91cm x 61cm)</span
+			>.
+		</p>
+		<h6 class="text-center">Submissions</h6>
+		<p class="w-5/6 m-auto">
+			A registered author can submit multiple abstracts, as long as that author is the presenting
+			author. Authors will also have an opportunity to submit and publish a full paper for MCBIOS
+			2024 proceedings in Frontiers in Artificial Intelligence. The deadline for full paper
+			submission is <span class="font-semibold">May 1, 2024</span>.
 		</p>
 	</div>
 </div>
