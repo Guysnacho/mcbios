30e1e2579c290234592f2251bd2cdf4d7332517c
Samuel Adetunji <guysnacho2@gmail.com>
2023-12-01 02:38:29 -0600

    Finish home page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -37,108 +37,111 @@ export const Nav = () => {
   });
 
   return (
-    <Box
-      w="90%"
-      h={75}
-      my={10}
-      mx="auto"
-      as={Flex}
-      shadow="lg"
-      boxShadow={10}
-      justify="space-between"
-      align="center"
-    >
-      <Link href="/">
-        <Heading as="h4">MCBIOS</Heading>
-      </Link>
+    <Box w="100%" bgColor="rgb(115,62,142,.95)">
+      <Box
+        color="white"
+        w="90%"
+        h={75}
+        mb={10}
+        mx="auto"
+        as={Flex}
+        shadow="lg"
+        boxShadow={10}
+        justify="space-between"
+        align="center"
+      >
+        <Link href="/">
+          <Heading as="h4">MCBIOS</Heading>
+        </Link>
 
-      {largeScreen ? (
-        <Tabs position="relative" variant="unstyled">
-          <TabList gap={15}>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">HOME</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">LEADERSHIP</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">MEMBERSHIP</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">CONFERENCES</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">EVENTS</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">PUBLICATIONS</Heading>
-              </Link>
-            </Tab>
-            <Tab>
-              <Link href="/">
-                <Heading as="h6">ABOUT</Heading>
-              </Link>
-            </Tab>
-          </TabList>
-          <TabIndicator
-            mt="-1.5px"
-            height="2px"
-            bg="blue"
-            borderRadius="1px"
-            defaultValue={pathMap[path] || 0}
-          />
-        </Tabs>
-      ) : (
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="nav-menu"
-            _expanded={{ shadow: "lg" }}
-          >
-            <HamburgerIcon fontSize="x-large" />
-          </MenuButton>
-          <MenuList gap={5} py={5} px={10} rounded={10} bgColor="#d3b8e0">
-            <MenuItem my={6} as="a" href="/">
-              HOME
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              LEADERSHIP
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              MEMBERSHIP
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              CONFERENCES
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              EVENTS
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              PUBLICATIONS
-            </MenuItem>
-            <Divider orientation="horizontal" />
-            <MenuItem my={6} as="a" href="/">
-              ABOUT
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      )}
+        {largeScreen ? (
+          <Tabs position="relative" variant="unstyled">
+            <TabList gap={15}>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">HOME</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">LEADERSHIP</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">MEMBERSHIP</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">CONFERENCES</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">EVENTS</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">PUBLICATIONS</Heading>
+                </Link>
+              </Tab>
+              <Tab>
+                <Link href="/">
+                  <Heading as="h6">ABOUT</Heading>
+                </Link>
+              </Tab>
+            </TabList>
+            <TabIndicator
+              mt="-1.5px"
+              height="2px"
+              bg="blue"
+              borderRadius="1px"
+              defaultValue={pathMap[path] || 0}
+            />
+          </Tabs>
+        ) : (
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="nav-menu"
+              _expanded={{ shadow: "lg" }}
+            >
+              <HamburgerIcon fontSize="x-large" />
+            </MenuButton>
+            <MenuList gap={5} py={5} px={10} rounded={10} bgColor="#d3b8e0">
+              <MenuItem my={6} as="a" href="/">
+                HOME
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                LEADERSHIP
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                MEMBERSHIP
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                CONFERENCES
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                EVENTS
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                PUBLICATIONS
+              </MenuItem>
+              <Divider orientation="horizontal" />
+              <MenuItem my={6} as="a" href="/">
+                ABOUT
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        )}
+      </Box>
     </Box>
   );
 };
