c035d46f9d5863b3001c0e86ccbde87c9637c815
Samuel Adetunji <guysnacho2@gmail.com>
2023-12-13 08:20:18 -0600

    Finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,9 @@ export default function Home() {
       <Divider as="hr" className="my-10 border-gray-500" />
 
       <section className="space-y-5 px-10 mx-auto md:w-5/6">
-        <h3>Greeting from the MCBIOS President:</h3>
+        <h3 className="underline text-center">
+          Greeting from the MCBIOS President:
+        </h3>
         <div className="md:flex my-5 gap-5 mx-auto">
           <Card>
             <CardHeader>
@@ -50,7 +52,7 @@ export default function Home() {
             </CardHeader>
             <CardFooter>
               <p className="text-sm">
-                Sincerely, Zhaohui “Steve” Qin, PhD
+                <strong className="mx-auto">Zhaohui “Steve” Qin, PhD</strong>
                 <br />
                 President, Mid-South Computational Biology & Bioinformatics
                 Society
@@ -122,111 +124,76 @@ export default function Home() {
 
       <Divider as="hr" className="my-10 border-gray-500" />
 
-      {/* <Box as={Stack} gap={5} mx="auto" w={[null, null, "90%"]}>
-        <Stack
-          direction={["column", null, null, "row"]}
-          gap={10}
-          my={5}
-          justify="center"
-          align="center"
-        >
-          <Image
-            src="/images/leadership/Qin-Steve.jpg"
-            alt="Qin Steve, MCBIOS President"
-            w={["50%", null, null, "20%"]}
-            objectFit="cover"
-          />
-        </Stack>
-        <Text as="p">
-          Sincerely, Zhaohui “Steve” Qin, PhD
-          <br />
-          President, Mid-South Computational Biology & Bioinformatics Society
-          <br />
-          Professor
-          <br />
-          Department of Biostatistics, Bioinformatics
-          <br />
-          Emory University
-        </Text>
-      </Box>
-
-      <Divider as="hr" className="my-10 border-gray-500" />
-
-      <Box mx="auto" w={[null, null, "90%"]}>
-        <Heading as="h6">Objectives</Heading>
-        <UnorderedList my={5}>
-          <ListItem>
+      <div className="w-4/5 md:w-2/3 mx-auto">
+        <h3 className="underline text-center">Objectives</h3>
+        <ul className="list-disc space-y-1 pl-5">
+          <li>
             Advance the understanding of bioinformatics and computational
             biology
-          </ListItem>
-          <ListItem>
+          </li>
+          <li>
             Bring together scientists of various backgrounds and disciplines
-          </ListItem>
-          <ListItem>
+          </li>
+          <li>
             Facilitate the collaboration of researchers with similar or
             complementary backgrounds to solve biological, health and/or medical
             problems
-          </ListItem>
-          <ListItem>
-            Promote education in bioinformatics and computational biology
-          </ListItem>
-          <ListItem>
+          </li>
+          <li>Promote education in bioinformatics and computational biology</li>
+          <li>
             Inform the general public on the results and implications of current
             research in bioinformatics and computational biology
-          </ListItem>
-          <ListItem>
+          </li>
+          <li>
             Promote other activities that will contribute to the development of
             bioinformatics and computational biology MCBIOS Bylaws Current
             version (Amended Feb 20, 2010)
-          </ListItem>
-        </UnorderedList>
-      </Box>
+          </li>
+        </ul>
+      </div>
 
       <Divider as="hr" className="my-10 border-gray-500" />
 
-      <SimpleGrid
-        columns={[1, null, null, 2]}
-        gap={5}
-        mx="auto"
-        w={[null, null, "90%"]}
-      >
-        <Box px={3}>
-          <Heading as="h6">Membership</Heading>
-          <Text as="p" my={5}>
+      <h3 className="text-center underline">Objectives</h3>
+      <div className="columns-1 lg:columns-2 w-5/6 mx-auto">
+        <div className="w-full items-center">
+          <h4 className="text-center">Membership</h4>
+          <p className="my-5">
             Membership in MBIOS includes meeting registration for the annual
             Conference and access to workshops and collaborative projects hosted
             by MCBIOS through the year. Students receive a discount for
             membership and are eligible for student awards. MCBIOS encourages
             involvement at both the regional and state level.
-          </Text>
-          <Box
-            my={15}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Button size="md" href="#" as={Link} colorScheme="accent">
-              Registration
+          </p>
+          <div className="w-full flex">
+            <Button
+              variant="shadow"
+              size="md"
+              className="my-4 mx-auto"
+              href="#"
+              color="secondary"
+            >
+              Become a Registration
             </Button>
-          </Box>
-        </Box>
-        <Box px={3}>
-          <Heading as="h6">Local Chapters</Heading>
-          <UnorderedList my={5} as="p">
-            <ListItem>
+          </div>
+        </div>
+        <div className="w-full mb-11">
+          <h4 className="text-center">Local Chapters</h4>
+          <ul className="list-disc space-y-1 pl-5 w-5/6 mx-auto">
+            <li>
               MCBIOS encourages the formation of local chapters. Chapters
               require:
-            </ListItem>
-            <ListItem>At least three members </ListItem>
-            <ListItem>An elected chairman </ListItem>
-            <ListItem>
+            </li>
+            <li>At least three members </li>
+            <li>An elected chairman </li>
+            <li>
               A regular meeting schedule, with a minimum of two meetings per
               year
-            </ListItem>
-            <ListItem>A purpose for organizing</ListItem>
-          </UnorderedList>
-        </Box>
-      </SimpleGrid> */}
+            </li>
+            <li>A purpose for organizing</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
