e458bfcf8947574cc3e233942563e361adfd16f1
Samuel Adetunji <guysnacho2@gmail.com>
2023-12-16 03:48:47 -0600

    Make board member card multipurpose

--- components/BoardMemberCard.tsx
+++ components/BoardMemberCard.tsx
@@ -4,30 +4,37 @@ import { Divider } from "@nextui-org/react";
 
 type BoardMemberCardProps = {
   name: string;
-  image: string;
+  image?: string;
   orgPosition?: string;
   title?: string;
   department?: string;
   affiliation?: string;
+  formerMember?: boolean;
+  index?: number;
 };
 const BoardMemberCard = (props: BoardMemberCardProps) => {
   return (
     <Card shadow="md" className="m-auto h-fit w-5/6 md:w-unit-7xl">
+      {props.index !== undefined ? (
+        <h6 className="mx-auto italic mt-2">#{props.index + 1}</h6>
+      ) : undefined}
       <CardHeader>
         <strong className="mx-auto text-lg">{props.name}</strong>
       </CardHeader>
       <Divider />
       <CardBody>
         <div className="sm:flex gap-5">
-          <Image
-            src={props.image}
-            fallbackSrc={props.image}
-            alt={props.name + " " + props.affiliation}
-            className="object-cover aspect-square object-top mx-auto w-52"
-            classNames={{
-              wrapper: ["mx-auto", "aspect-square", "mb-5", "sm:mb-0"],
-            }}
-          />
+          {props.image ? (
+            <Image
+              src={props.image}
+              fallbackSrc={props.image}
+              alt={props.name + " " + props.affiliation}
+              className="object-cover aspect-square object-top mx-auto w-52"
+              classNames={{
+                wrapper: ["mx-auto", "aspect-square", "mb-5", "sm:mb-0"],
+              }}
+            />
+          ) : undefined}
           <div className="text-medium my-auto">
             {props.orgPosition ? (
               <p className="font-bold">
