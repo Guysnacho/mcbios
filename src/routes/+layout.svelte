b25d6586eb809e947e5715e8546743456d84f1c3
Guysnacho <guysnacho2@gmail.com>
2023-12-18 12:14:41 -0600

    Add homepage and fix static imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,109 @@
 <svelte:head>
 	<title>MCBIOS 2022</title>
-	<!-- <script src="./assets/bundle.js.download"></script> -->
-
 	<!-- Vendor CSS Files -->
-	<!-- <link href="./assets/bundle.css" rel="stylesheet" /> -->
+	<link rel="stylesheet" href="./assets/style/bootstrap.min.css" />
+	<!-- Ionic Icon Css -->
+	<link rel="stylesheet" href="./assets/style/ionicons.min.css" />
+	<!-- animate.css -->
+	<link rel="stylesheet" href="./assets/style/animate.css" />
+	<!-- Magnify Popup -->
+	<link rel="stylesheet" href="./assets/style/magnific-popup.css" />
+	<!-- Owl Carousel CSS -->
+	<link rel="stylesheet" href="./assets/style/slick.css" />
+	<!-- Main Stylesheet -->
+	<link rel="stylesheet" href="./assets/style.css" />
 
-	<!-- <link href="./assets/style.css" rel="stylesheet" /> -->
-	<!-- Template Main CSS File -->
+	<script src="./assets/js_bundle/ionicons.esm.js.download" type="module"></script>
+	<script src="./assets/js_bundle/ionicons.js.download"></script>
+	<script
+		src="./assets/js_bundle/common.js.download"
+		type="text/javascript"
+		charset="UTF-8"
+	></script>
+	<script src="./assets/js_bundle/util.js.download" type="text/javascript" charset="UTF-8"></script>
 </svelte:head>
 
+<!-- Header Start -->
+<header class="navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<!-- header Nav Start -->
+				<nav class="navbar">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button
+								type="button"
+								class="navbar-toggle collapsed"
+								data-toggle="collapse"
+								data-target="#bs-example-navbar-collapse-1"
+							>
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar" />
+								<span class="icon-bar" />
+								<span class="icon-bar" />
+							</button>
+							<a class="navbar-brand" href="http://2022.mcbios.com/index.html">
+								<img
+									src="./assets/images/MCBIOS_Logo2.png"
+									alt="Logo"
+									height="85"
+									style="margin-top: -15px;"
+								/>
+							</a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="http://2022.mcbios.com/index.html">Home</a></li>
+								<li>
+									<a href="http://2022.mcbios.com/abstract-submission.html">Abstract Submission</a>
+								</li>
+								<li><a href="http://2022.mcbios.com/about-mcbios.html">About Us</a></li>
+								<li>
+									<a href="http://2022.mcbios.com/ysea.html">Young Scientist Excellence Awards</a>
+								</li>
+								<li />
+								<li><a href="http://2022.mcbios.com/program.php">Program</a></li>
+								<li />
+								<li><a href="http://2022.mcbios.com/registration.php">Registration</a></li>
+							</ul>
+						</div>
+						<!-- /.navbar-collapse -->
+					</div>
+					<!-- /.container-fluid -->
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+
 <slot />
+
+<!-- footer Start -->
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="footer-manu">
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li>
+							<a href="/abstract-submission">Abstract Submissions</a>
+						</li>
+						<li><a href="/about-mcbios">About Us</a></li>
+						<li>
+							<a href="/ysea">Young Scientist Excellence Awards</a>
+						</li>
+						<li><a href="/program">Program</a></li>
+						<li><a href="/registration">Registration</a></li>
+					</ul>
+				</div>
+				<!--<p class="copyright">Copyright 2018 &copy; Design & Developed by <a href="http://www.themefisher.com">themefisher.com</a>. All rights reserved.-->
+				<br />
+				<p />
+			</div>
+		</div>
+	</div>
+</footer>
