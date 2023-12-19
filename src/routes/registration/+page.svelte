d7aadafdb1d1caba4f003187ef90160e8d62e42b
Guysnacho <guysnacho2@gmail.com>
2023-12-18 19:18:29 -0600

    Add registration page

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -8,4 +8,51 @@
 
 <main>
 	<Hero bannerText="REGISTRATION" />
+
+	<section class="pricing-table section bg-white">
+		<div class="container">
+			<h2 style="float: left">YSEA Registration Deadline: March 31, 2022</h2>
+			<h2 style="float: right">General Registration Deadline: April 28, 2022</h2>
+
+			<!-- End row -->
+		</div>
+		<h3 class="text-center">Registration is Closed</h3>
+		<!-- End container -->
+	</section>
+	<!-- End section -->
+	<section class="faq section-sm">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-center">Information Regarding Registration</h2>
+				</div>
+			</div>
+			<div class="row mt-30">
+				<div class="col-md-6">
+					<h4>Do I need to purchase multiple slots to attend all events?</h4>
+					<p>
+						One registration fee provides access to all sessions and activities at the joint
+						conference
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h4>How can I access the actual registration site?</h4>
+					<p>Click on the "start registration" button below your preferred registration rate.</p>
+				</div>
+				<div class="col-md-6">
+					<h4>Is payment due now or at the time of the conference?</h4>
+					<p>Payment is expected at the time of registration (Credit or Debit Card).</p>
+				</div>
+				<div class="col-md-6">
+					<h4>Why do you need to collect certain information from me?</h4>
+					<p>
+						Demographic information collected during the registration process is required as part of
+						the grant applications that have helped to defray some of the costs associated with
+						putting on the conference. Registrant information collected is never shared or sold to
+						third parties.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
