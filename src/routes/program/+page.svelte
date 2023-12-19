d351a8e17c7bd283564ae6d9cd4c45252fc4de5f
Guysnacho <guysnacho2@gmail.com>
2023-12-18 19:04:55 -0600

    Add  rerouting program page

--- src/routes/program/+page.svelte
+++ src/routes/program/+page.svelte
@@ -8,4 +8,21 @@
 
 <main>
 	<Hero bannerText="PROGRAM" />
+
+	<section class="pricing-table section bg-white">
+		<div class="container">
+			<b
+				>If the above element does not load, please click this link:
+				<a
+					style="text-decoration: underline"
+					href="https://whova.com/embedded/event/hemjc_202204/?utc_source=ems"
+					target="">https://whova.com/embedded/event/hemjc_202204/?utc_source=ems</a
+				>.
+			</b>
+			<b> <!-- End row --> </b>
+		</div>
+		<b> <!-- End container --> </b>
+	</section>
+	<script type="text/javascript" src="./assets/form/agendawidget.js.download"></script>
+	<b> <!-- End section --> </b>
 </main>
