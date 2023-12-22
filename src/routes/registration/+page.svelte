d69c9be78c9059326f1c412b881e62e7276ab74b
Guysnacho <guysnacho2@gmail.com>
2023-12-22 01:54:59 -0600

    Add registration pages

---
+++ src/routes/registration/+page.svelte
@@ -0,0 +1,303 @@
+<main id="main" class="site-main">
+	<article
+		itemtype="https://schema.org/CreativeWork"
+		itemscope
+		id="post-13"
+		class="post-13 page type-page status-publish ast-article-single"
+	>
+		<header class="entry-header ast-header-without-markup" />
+		<!-- .entry-header -->
+
+		<div class="entry-content clear" itemprop="text">
+			<div
+				data-elementor-type="post"
+				data-elementor-id="13"
+				class="elementor elementor-13"
+				data-elementor-settings="[]"
+			>
+				<div class="elementor-inner">
+					<div class="elementor-section-wrap">
+						<section
+							class="elementor-element elementor-element-b006fe5 elementor-section-content-middle elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+							data-id="b006fe5"
+							data-element_type="section"
+							data-settings={{ background_background: 'classic' }}
+						>
+							<div class="elementor-background-overlay" />
+							<div class="elementor-container elementor-column-gap-default">
+								<div class="elementor-row">
+									<div
+										class="elementor-element elementor-element-67b5334 elementor-column elementor-col-100 elementor-top-column"
+										data-id="67b5334"
+										data-element_type="column"
+									>
+										<div class="elementor-column-wrap elementor-element-populated">
+											<div class="elementor-widget-wrap">
+												<div
+													class="elementor-element elementor-element-6c248cf elementor-widget elementor-widget-text-editor"
+													data-id="6c248cf"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix" />
+													</div>
+												</div>
+												<section
+													class="elementor-element elementor-element-4ac1d70 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section"
+													data-id="4ac1d70"
+													data-element_type="section"
+												>
+													<div class="elementor-container elementor-column-gap-wider">
+														<div class="elementor-row">
+															<div
+																class="elementor-element elementor-element-b6aa1e8 elementor-column elementor-col-100 elementor-inner-column"
+																data-id="b6aa1e8"
+																data-element_type="column"
+															>
+																<div class="elementor-column-wrap elementor-element-populated">
+																	<div class="elementor-widget-wrap">
+																		<div
+																			class="elementor-element elementor-element-5240173 elementor-widget elementor-widget-heading"
+																			data-id="5240173"
+																			data-element_type="widget"
+																			data-widget_type="heading.default"
+																		>
+																			<div
+																				class="elementor-widget-container"
+																				style="margin-top: 4rem;"
+																			>
+																				<h1
+																					style="color: black;text-align: center;"
+																					class="elementor-heading-title elementor-size-default"
+																				>
+																					Registration
+																				</h1>
+																			</div>
+																		</div>
+																		<div
+																			class="elementor-element elementor-element-84fc0fe elementor-align-center elementor-mobile-align-center elementor-widget elementor-widget-button"
+																			data-id="84fc0fe"
+																			data-element_type="widget"
+																			data-widget_type="button.default"
+																		>
+																			<div class="elementor-widget-container">
+																				<div class="elementor-button-wrapper">
+																					<a
+																						href="https://www.mcbios.com/membership/"
+																						class="elementor-button-link elementor-button elementor-size-sm elementor-animation-shrink"
+																						role="button"
+																					>
+																						<span class="elementor-button-content-wrapper">
+																							<span class="elementor-button-text">Register Now</span
+																							>
+																						</span>
+																					</a>
+																				</div>
+																			</div>
+																		</div>
+																		<div
+																			class="elementor-element elementor-element-20ff755 elementor-widget elementor-widget-text-editor"
+																			data-id="20ff755"
+																			data-element_type="widget"
+																			data-widget_type="text-editor.default"
+																		>
+																			<div class="elementor-widget-container">
+																				<div class="elementor-text-editor elementor-clearfix">
+																					<p>&nbsp;</p>
+																					<p>&nbsp;</p>
+																					<p>&nbsp;</p>
+																					<p>&nbsp;</p>
+																					<p>&nbsp;</p>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</section>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section
+							class="elementor-element elementor-element-6c1b574 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+							data-id="6c1b574"
+							data-element_type="section"
+						>
+							<div class="elementor-container elementor-column-gap-default">
+								<div class="elementor-row">
+									<div
+										class="elementor-element elementor-element-941c9cf elementor-column elementor-col-100 elementor-top-column"
+										data-id="941c9cf"
+										data-element_type="column"
+									>
+										<div class="elementor-column-wrap elementor-element-populated">
+											<div class="elementor-widget-wrap">
+												<div
+													class="elementor-element elementor-element-c23fe0b elementor-widget elementor-widget-spacer"
+													data-id="c23fe0b"
+													data-element_type="widget"
+													data-widget_type="spacer.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-spacer">
+															<div class="elementor-spacer-inner" />
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-1b8940f elementor-widget elementor-widget-heading"
+													data-id="1b8940f"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black;text-align: center;"
+															class="elementor-heading-title elementor-size-default"
+														>
+															Travel Support
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-67e34c3 elementor-widget elementor-widget-text-editor"
+													data-id="67e34c3"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix">
+															Limited number of student/trainee travel stipends&nbsp;(including a
+															registration fee waiver and/or lodging support)&nbsp;are available to
+															those who apply and&nbsp;register by January 31, 2019.
+															Students/trainees who are submitting an abstract for oral or poster
+															presentation are invited to apply for a stipend to help defray the
+															cost of attending the conference. Stipends will be offered on a
+															first-come-first-served basis and&nbsp;priority&nbsp;will be given to
+															those selected for&nbsp;platform presentations. Students with
+															institutional support are not eligible for these funds. Expenses that
+															are not supported by travel stipend will be the responsibility of the
+															attendee. Travel awards will be announced March 1, 2019. As we work
+															with sponsors, additional awards may become available to paid
+															registrants to present high-quality work, which is determined by the
+															MCBIOS ’19 Fellowship and Award Committee
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-c293bef elementor-widget elementor-widget-heading"
+													data-id="c293bef"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black;text-align: center;"
+															class="elementor-heading-title elementor-size-default"
+														>
+															Conference Fees
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-2162f50 elementor-widget elementor-widget-text-editor"
+													data-id="2162f50"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix">
+															<p>
+																<a href="/key-dates"
+																	>Click here for registration deadline information</a
+																>&nbsp;
+															</p>
+															<p>
+																<strong style="color: black"
+																	>Fees below are for early registration before the registration
+																	deadline.</strong
+																>
+															</p>
+															<p>
+																Professional- $375<br />Professional Postdoc- $250<br />Student-
+																$175
+															</p>
+															<p>
+																<strong style="color: black"
+																	>Fees below are for registration after the early registration
+																	deadline.&nbsp;</strong
+																>
+															</p>
+															<p>
+																Professional- $475<br />Professional Postdoc- $350<br />Student-$175
+															</p>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section
+							class="elementor-element elementor-element-3549abe elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+							data-id="3549abe"
+							data-element_type="section"
+						>
+							<div class="elementor-container elementor-column-gap-wide">
+								<div class="elementor-row">
+									<div
+										class="elementor-element elementor-element-7e54d2f elementor-column elementor-col-100 elementor-top-column"
+										data-id="7e54d2f"
+										data-element_type="column"
+									>
+										<div class="elementor-column-wrap elementor-element-populated">
+											<div class="elementor-widget-wrap">
+												<div
+													class="elementor-element elementor-element-ba4bcbe elementor-widget elementor-widget-spacer"
+													data-id="ba4bcbe"
+													data-element_type="widget"
+													data-widget_type="spacer.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-spacer">
+															<div class="elementor-spacer-inner" />
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-96d4d6a elementor-widget elementor-widget-text-editor"
+													data-id="96d4d6a"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container" style="margin-inline: auto;">
+														<div class="elementor-text-editor elementor-clearfix">
+															<p style="text-align: center;font-style: oblique;">
+																Banner photo Credit: the Greater Birmingham Convention &amp;
+																Visitors Bureau and UAB
+															</p>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- .entry-content .clear -->
+	</article>
+	<!-- #post-## -->
+</main>
---
+++ src/routes/registration/instructions/+page.svelte
@@ -0,0 +1,334 @@
+<main id="main" class="site-main">
+	<article
+		itemtype="https://schema.org/CreativeWork"
+		itemscope
+		id="post-562"
+		class="post-562 page type-page status-publish ast-article-single"
+	>
+		<header class="entry-header ast-header-without-markup" />
+		<!-- .entry-header -->
+
+		<div class="entry-content clear" itemprop="text">
+			<div
+				data-elementor-type="post"
+				data-elementor-id="562"
+				class="elementor elementor-562"
+				data-elementor-settings="[]"
+			>
+				<div class="elementor-inner">
+					<div class="elementor-section-wrap">
+						<section
+							class="elementor-element elementor-element-833de60 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+							data-id="833de60"
+							data-element_type="section"
+							data-settings={{ background_background: 'classic' }}
+						>
+							<div class="elementor-container elementor-column-gap-default">
+								<div class="elementor-row">
+									<div
+										class="elementor-element elementor-element-3a3ca427 elementor-column elementor-col-100 elementor-top-column"
+										data-id="3a3ca427"
+										data-element_type="column"
+									>
+										<div
+											class="elementor-column-wrap elementor-element-populated"
+											style="background-color: #23a455;height: 30vh;"
+										>
+											<div class="elementor-widget-wrap">
+												<div
+													class="elementor-element elementor-element-195ed71 elementor-widget elementor-widget-heading"
+													data-id="195ed71"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div
+														class="elementor-widget-container"
+														style="align-items: center;justify-items: center;margin-top:10vh"
+													>
+														<h2
+															class="elementor-heading-title elementor-size-default"
+															style="color:white;text-align: center;"
+														>
+															Conference Registration and Abstract Submission
+														</h2>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section
+							class="elementor-element elementor-element-9c3b058 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+							data-id="9c3b058"
+							data-element_type="section"
+						>
+							<div class="elementor-container elementor-column-gap-default">
+								<div class="elementor-row">
+									<div
+										class="elementor-element elementor-element-07c5b89 elementor-column elementor-col-100 elementor-top-column"
+										data-id="07c5b89"
+										data-element_type="column"
+									>
+										<div class="elementor-column-wrap elementor-element-populated">
+											<div class="elementor-widget-wrap">
+												<div
+													class="elementor-element elementor-element-5a9a478 elementor-widget elementor-widget-spacer"
+													data-id="5a9a478"
+													data-element_type="widget"
+													data-widget_type="spacer.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-spacer">
+															<div class="elementor-spacer-inner" />
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-319ee8d elementor-widget elementor-widget-heading"
+													data-id="319ee8d"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black"
+															class="elementor-heading-title elementor-size-default"
+														>
+															Registration for the 2019 MCBIOS Conference and Workshop
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-2a783c8 elementor-widget elementor-widget-text-editor"
+													data-id="2a783c8"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix">
+															<ul>
+																<li>
+																	Choose the registration type (Student, Post-doc, or
+																	Professional)&nbsp;from the drop-down menu and click “Register.”
+																</li>
+																<li>
+																	Payment is expected at the time of registration&nbsp;using PayPal.
+																	Alternatively, you may request&nbsp;an invoice to be sent to you
+																	or your Institution.
+																</li>
+																<li>
+																	This year, the conference workshops are included in the meeting
+																	registration. Please&nbsp;choose one workshop during
+																	registration.&nbsp;
+																</li>
+																<li>
+																	Demographic information gathered during registration will be
+																	aggregated and utilized by MCBIOS for grant applications. Member’s
+																	information is never shared or sold.
+																</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-c73c29b elementor-widget elementor-widget-heading"
+													data-id="c73c29b"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black"
+															class="elementor-heading-title elementor-size-default"
+														>
+															2019 MCBIOS Abstract Submission Instructions
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-929a1af elementor-widget elementor-widget-text-editor"
+													data-id="929a1af"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix">
+															<ol>
+																<li>
+																	You must be&nbsp;registered before submitting&nbsp;an abstract.
+																</li>
+																<li>
+																	Please use the <a
+																		href="https://mcbios.org/conference-registration-and-abstract-submission"
+																		>EasyChair Portal</a
+																	>
+																	to submit an abstract and indicate the following:
+																	<ul>
+																		<li>
+																			<span class="gD"
+																				>Indicate presentation type (Oral or&nbsp;Poster)</span
+																			>
+																		</li>
+																		<li><span class="gD">Indicate session preference</span></li>
+																		<li>
+																			<span class="gD"
+																				>Membership Type (Professional, Post-Doc, Student)</span
+																			>
+																		</li>
+																		<li>
+																			<span class="gD"
+																				>Indicate whether Travel Assistantship Stipend is needed
+																				(only applies&nbsp;to student&nbsp;and post-doc categories)</span
+																			>
+																		</li>
+																		<li>
+																			<span class="gD"
+																				>Indicate if competing for the “MCBIOS Young Scientist
+																				Excellence Award” (only applies to student&nbsp;and post-doc
+																				categories). If this option is slected, provide additional
+																				information for evaluation: Innovation in research, author’s
+																				contribution to the research, and statement of academic
+																				integrity.</span
+																			>
+																		</li>
+																	</ul>
+																</li>
+																<li>
+																	The <strong>Abstract </strong>should not exceed 300 words. Please
+																	minimize the use of abbreviations and do not cite references in
+																	the abstract. The abstract must include the following separate
+																	sections:
+																	<ul>
+																		<li>
+																			<strong>Background:</strong> the context and purpose of the study
+																		</li>
+																		<li><strong>Results:</strong> the main findings</li>
+																		<li>
+																			<strong>Conclusions:</strong> a brief summary and potential implications
+																		</li>
+																	</ul>
+																</li>
+																<li>
+																	There are a limited number of spots for oral presentations. If not
+																	selected for oral presentation, you will automatically be
+																	considered for poster presentation. Oral presentations will be
+																	10-15&nbsp;min long including questions, depending on the session
+																	program designed by the session chair.&nbsp;Poster size
+																	limitation: 30″W x 40″H (122cm x 91cm).
+																</li>
+																<li>
+																	A registered author can submit multiple abstracts, as long as that
+																	author is the presenting author.
+																</li>
+																<li>
+																	<span class="gD"
+																		>Consider publishing full paper for MCBIOS 2019 proceedings in
+																		BMC Bioinformatics. The deadline for full paper submission is
+																		May 1, 2019.</span
+																	>
+																</li>
+															</ol>
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-e8f79e4 elementor-widget elementor-widget-heading"
+													data-id="e8f79e4"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black"
+															class="elementor-heading-title elementor-size-default"
+														>
+															Student Travel Stipends
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-c20553b elementor-widget elementor-widget-text-editor"
+													data-id="c20553b"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div class="elementor-text-editor elementor-clearfix">
+															Limited number of student/trainee travel stipends (including a
+															registration fee waiver and/or lodging support) are available to those
+															who apply and register by January 31, 2019. Students/trainees who are
+															submitting an abstract for oral or poster presentation are invited to
+															apply for a stipend to help defray the cost of attending the
+															conference. Stipends will be offered on a first-come-first-served
+															basis and priority will be given to those selected for platform
+															presentations. Students with institutional support are not eligible
+															for these funds. Expenses that are not supported by travel stipend
+															will be the responsibility of the attendee. Travel awards will be
+															announced March 1, 2019. As we work with sponsors, additional awards
+															may become available to paid registrants to present high-quality work,
+															which is determined by the MCBIOS ’19 Fellowship and Award Committee.
+														</div>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-0025462 elementor-widget elementor-widget-heading"
+													data-id="0025462"
+													data-element_type="widget"
+													data-widget_type="heading.default"
+												>
+													<div class="elementor-widget-container">
+														<h3
+															style="color: black"
+															class="elementor-heading-title elementor-size-default"
+														>
+															MCBIOS Young Scientist Excellence Award
+														</h3>
+													</div>
+												</div>
+												<div
+													class="elementor-element elementor-element-ddc69cf elementor-widget elementor-widget-text-editor"
+													data-id="ddc69cf"
+													data-element_type="widget"
+													data-widget_type="text-editor.default"
+												>
+													<div class="elementor-widget-container">
+														<div
+															class="elementor-text-editor elementor-clearfix"
+															style="margin-bottom: 10rem;;"
+														>
+															“MCBIOS Young Scientist Excellence” awards program recognizes students
+															and postdoctoral fellows that exhibit scientific excellence in the
+															field of Bioinformatics. Applications from students and postdoctoral
+															fellows will be rigorously evaluated. The top five candidates will be
+															invited to give an oral presentation in a session dedicated to this
+															award program. In addition to an abstract, participation in this
+															program requires submission of separate description of the innovation
+															of the research and the individual’s contribution to the work being
+															presented. Selection of the top five candidates is based on the
+															evaluation of the applications by the MCBIOS board members for quality
+															and impact of the research. The finalists are selected by a panel of
+															judges (including keynote speakers) after the oral presentations. The
+															quality of the professional presentation is the primary consideration
+															for selection of the award recipients. Applicants with demonstrated
+															multidisciplinary contribution and initiative are given preference
+															during final selection. Announcement of the awards will be made at
+															during the final luncheon.
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- .entry-content .clear -->
+	</article>
+	<!-- #post-## -->
+</main>
