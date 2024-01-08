7d743198c69002604dbc868bd94632f097254557
Samuel Adetunji <guysnacho2@gmail.com>
2024-01-08 17:07:15 -0600

    Remove extra footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -147,35 +147,6 @@
 </main>
 <!-- End #main -->
 
-<!-- ======= Footer ======= -->
-<footer id="footer">
-	<div class="footer-top">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-6">
-					<h3>MCBIOS 2023</h3>
-					<p>
-						The 19th Annual Meeting of MCBIOS will be held on the campus of University of Dallas <br
-						/>with the theme of "Big Data and Artificial Intelligence for Genomics and Therapeutics"
-						from March 15-17, 2023.
-					</p>
-				</div>
-			</div>
-
-			<div class="social-links">
-				<a
-					href="https://www.facebook.com/pages/category/Organization/MidSouth-Computational-Biology-and-Bioinformatics-Society-MCBIOS-247738778611609/"
-					class="facebook"><i class="bx bxl-facebook" /></a
-				>
-				<a href="https://www.linkedin.com/groups/9069670/" class="linkedin"
-					><i class="bx bxl-linkedin" /></a
-				>
-			</div>
-		</div>
-	</div>
-</footer>
-<!-- End Footer -->
-
 <a
 	href="http://2023.mcbios.com/#"
 	class="back-to-top d-flex align-items-center justify-content-center active"
