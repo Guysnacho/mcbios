c207913d94d4caf482ae060cfb1e0e9f647ebf8f
Samuel Adetunji <guysnacho2@gmail.com>
2024-01-11 15:55:52 -0600

    Add some flair to hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -9,7 +9,11 @@
 		<div
 			class="w-full py-3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-900 bg-opacity-40"
 		>
-			<h1 class="h1 fill-token select-none drop-shadow-sm">{headerText}</h1>
+			<h1
+				class="h1 py-4 fill-token select-none drop-shadow-sm bg-gradient-to-br from-secondary-500 to-tertiary-100 bg-clip-text text-transparent box-decoration-clone"
+			>
+				{headerText}
+			</h1>
 		</div>
 	</header>
 </div>
