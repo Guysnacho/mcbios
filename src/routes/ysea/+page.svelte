60d2067e1473bbfde7ea079579b85e171d4439b7
Samuel Adetunji <guysnacho2@gmail.com>
2024-02-20 02:16:28 -0600

    Add gradient to announcement blocks

--- src/routes/ysea/+page.svelte
+++ src/routes/ysea/+page.svelte
@@ -10,7 +10,9 @@
 <div class="w-full justify-center">
 	<Hero imageSrc={AwardPic} headerText="MCBIOS YOUNG SCIENTIST EXCELLENCE AWARDS" />
 </div>
-<div class="w-full justify-center bg-surface-700 shadow-lg py-10 -mt-16">
+<div
+	class="w-full justify-center bg-gradient-to-tr from-surface-600 to-surface-800 shadow-lg py-10 -mt-16"
+>
 	<div class="space-y-5 w-11/12 md:w-5/6 mx-auto">
 		<p class="text-center blockquote">
 			If you are a graduate student or a postdoc, we invite you to apply. If you are a mentor of
@@ -18,16 +20,73 @@
 		</p>
 	</div>
 </div>
-<div class="w-5/6 mx-auto mt-32 mb-10 block md:flex gap-5">
+
+<div class="w-5/6 mx-auto my-10">
+	<h3 class="h3 underline mb-5 text-center">Finalists have been selected!</h3>
+	<p class="text-center">Selections will be made during the following sessions</p>
+	<div class="w-5/6 md:w-2/3 lg:w-2/5 mx-auto my-5 space-y-11">
+		<div class="outline outline-tertiary-600 rounded-md p-5">
+			<h4 class="h4 text-center">YSEA Session 1</h4>
+			<p class="italic text-center">Undergrad Presentations | 1 winners to be selected</p>
+			<ul class="list my-3">
+				<li><span>Tri Ha</span></li>
+				<li><span>Sophia Palafox</span></li>
+			</ul>
+
+			<h5 class="h5 text-center">Postdoc Presentations</h5>
+			<p class="italic text-center">Postdoc Presentations | 1 winners to be selected</p>
+			<ul class="list my-3">
+				<li><span>Yeganeh Madadi</span></li>
+				<li><span>Tyrome Sweet</span></li>
+			</ul>
+		</div>
+
+		<div class="outline outline-tertiary-600 rounded-md p-5">
+			<h4 class="h4 text-center">YSEA Session 2</h4>
+			<p class="italic text-center">Graduate student Presentations | 3 winners to be selected</p>
+			<ul class="list my-3">
+				<li><span>David Otohinoyi</span></li>
+				<li><span>Adeolu Oluremi</span></li>
+				<li><span>Jiawen Chen</span></li>
+				<li><span>Amin Akbari Ahangar</span></li>
+				<li><span>JaMor Hairston</span></li>
+				<li><span>Jiaying Lu</span></li>
+				<li><span>Rahmatullah Roche</span></li>
+				<li><span>Santhosh Kumar Karthikeyan</span></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div
+	class="w-full justify-center bg-gradient-to-tr from-surface-600 to-surface-800 shadow-lg py-10"
+>
+	<div class="space-y-5 w-11/12 md:w-5/6 mx-auto">
+		<p>
+			Trainees will be presenting in 2 separate but concurrent sessions in 2 different rooms.<br />
+			Candidates who aren't selected as finalists will have the opportunity to either present a poster
+			or deliver a lightning talk elsewhere in during the conference.
+		</p>
+		<p class="blockquote">
+			Session 1 would be March 22nd, 4pm-5:00pm while Session 2 would be March 22nd, 4pm-5:30pm.
+		</p>
+	</div>
+</div>
+
+<div class="w-5/6 mx-auto my-10" />
+
+<div class="w-1/4 my-24 mx-auto">
+	<hr class="!border-t-8 !border-double" />
+</div>
+
+<div class="w-5/6 mx-auto mb-10 block md:flex gap-5">
 	<div class="w-full md:w-2/5">
 		<h3 class="h3 underline mb-5 text-center">
 			2024 Young Scientist Excellence Award Applications
 		</h3>
 		<p class="text-center">Deadline: January 31st, 2024</p>
 		<div class="px-auto flex">
-			<a href="#" class="btn btn-lg variant-outline-primary my-7 mx-auto"
-				>Apply Closed</a
-			>
+			<a href="#" class="btn btn-lg variant-outline-primary my-7 mx-auto">Application Closed</a>
 		</div>
 	</div>
 	<div class="w-full mt-10 md:w-3/5 md:mt-0 space-y-3">
