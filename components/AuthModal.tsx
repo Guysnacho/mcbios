708b8c3205f989dedad1d45c34cbadcd90c84fbc
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-26 01:06:20 -0500

    Add loading, email, password state and signin query

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -9,51 +9,94 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import { SupabaseClient } from "@supabase/supabase-js";
-import React, { Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/navigation";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
-export const AuthModal = (props: {
+export const AuthModal = ({
+  isOpen,
+  setIsOpen,
+  supabase,
+}: {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
   supabase: SupabaseClient<Database>;
 }) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { onClose } = useDisclosure();
+
+  const handleLogin = async () => {
+    setLoading(true);
+    if (email && password) {
+      // const { data, error } = await supabase.auth.signUp({ email, password, options: {} });
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        setError(error.message);
+      } else {
+        setError("");
+        router.push("/dashboard");
+      }
+    } else {
+      setEmail("");
+      setPassword("");
+      setError("Invalid email or password provided");
+    }
+    setLoading(false);
+  };
 
   return (
     <Modal
       size="lg"
-      isOpen={props.isOpen}
+      isOpen={isOpen}
       onClose={() => {
-        props.setIsOpen(false);
+        setIsOpen(false);
         onClose();
       }}
     >
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1 text-xl">
-              Member Login
-            </ModalHeader>
+            <ModalHeader className="flex flex-col gap-1 text-xl"></ModalHeader>
             <ModalBody>
+              {error ? (
+                <blockquote className="blockquote">{error}</blockquote>
+              ) : undefined}
+
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
                 id="email"
+                onChange={(e) => setEmail(e.currentTarget.value)}
                 placeholder="abcd@university.edu"
+                disabled={loading}
               />
               <label htmlFor="email">Password</label>
               <input
                 type="password"
                 name="password"
                 id="password"
+                onChange={(e) => setPassword(e.currentTarget.value)}
                 placeholder="top secret password"
+                disabled={loading}
               />
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
+              <Button
+                color="danger"
+                variant="light"
+                onPress={onClose}
+                disabled={loading}
+              >
                 Cancel
               </Button>
-              <Button color="primary" onPress={onClose}>
+              <Button color="primary" onPress={handleLogin} disabled={loading}>
                 Login
               </Button>
             </ModalFooter>
