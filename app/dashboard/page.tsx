7a921ac905f495ab35bc63752e5e204448d2aae0
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-26 01:56:59 -0500

    Buffer up the dash

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
         <body class="h-full">
         ```
       */}
-        <main>
+        <main className="h-64">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h3>Dashboard</h3>
           </div>
