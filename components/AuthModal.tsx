11ca1c6ac899b7afc6f1e9fc36253d028e359c70
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-25 23:34:22 -0500

    Add empty auth modal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -27,33 +27,31 @@ export const AuthModal = (props: {
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
-              Modal Title
+            <ModalHeader className="flex flex-col gap-1 text-xl">
+              Member Login
             </ModalHeader>
             <ModalBody>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Magna exercitation reprehenderit magna aute tempor cupidatat
-                consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                aliqua enim laboris do dolor eiusmod.
-              </p>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="abcd@university.edu"
+              />
+              <label htmlFor="email">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="top secret password"
+              />
             </ModalBody>
             <ModalFooter>
               <Button color="danger" variant="light" onPress={onClose}>
-                Close
+                Cancel
               </Button>
               <Button color="primary" onPress={onClose}>
-                Action
+                Login
               </Button>
             </ModalFooter>
           </>
