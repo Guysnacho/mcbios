80623ed4cea38ecb55785f49d9401096df11705e
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-25 23:24:21 -0500

    Add empty dashboard and fix middleware route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Reports", href: "#", current: false },
+];
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="min-h-full bg-gray-100">
+        {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+        <main>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h3>Dashboard</h3>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
