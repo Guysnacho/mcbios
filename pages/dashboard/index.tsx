a90f5abd4064154f0e04b9b5f4c8554e6b17849a
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-31 22:03:25 -0500

    Fetch current user info on dash

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,15 +1,41 @@
 // get serverside props here with videos and whatnot. Profile info updates and video content
 
+import { createClient } from "@/lib/utils/supabase/server-props";
+import { Database } from "@/lib/utils/supabase/types";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
 
-export default function Dashboard() {
+type User =
+  | {
+      user: Database["public"]["Tables"]["member"]["Row"];
+    }
+  | {
+      user: undefined;
+    };
+
+export const getServerSideProps = (async (
+  context: GetServerSidePropsContext
+) => {
+  // Fetch data from external API
+  const client = createClient(context);
+  const { data } = await client.from("member").select("*").single();
+
+  if (data) {
+    return { props: { user: data } };
+  }
+  return { props: { user: undefined } };
+}) satisfies GetServerSideProps<User>;
+
+export default function Dashboard(props: User) {
   return (
     <>
       <Head>
         <title>MCBIOS Videos</title>
         <meta content="MCBIOS Videos | MidSouth Computational Biology and Bioinformatics Society" />
       </Head>
-      <h3>Dashboard Content</h3>
+      <div className="container mx-auto justify-center">
+        <h3 className="text-center">Welcome {props.user?.fname}</h3>
+      </div>
     </>
   );
 }
