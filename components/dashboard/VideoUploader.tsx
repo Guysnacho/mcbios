0b124e475da63517a2fb232c9942d23e10c01921
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-03 03:25:52 -0500

    Switch to db ref save

--- components/dashboard/VideoUploader.tsx
+++ components/dashboard/VideoUploader.tsx
@@ -1,7 +1,19 @@
 import { createClient } from "@/lib/utils/supabase/component";
+import { Database } from "@/lib/utils/supabase/types";
 import { uploadFile } from "@/lib/utils/supabase/upload";
 import { getLocalTimeZone, now, parseDate } from "@internationalized/date";
-import { Button, DatePicker, DateValue, Input } from "@nextui-org/react";
+import {
+  Button,
+  DatePicker,
+  DateValue,
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 import { useDateFormatter } from "@react-aria/i18n";
 import { useEffect, useState } from "react";
 
@@ -10,8 +22,10 @@ export const VideoUploader = () => {
   const [dateError, setDateError] = useState("");
   const [title, setTitle] = useState("");
   const [titleError, setTitleError] = useState("");
-  const [video, setVideo] = useState<File | null>();
+  const [video, setVideo] = useState("");
   const [videoError, setVideoError] = useState("");
+  const [vidList, setVidList] =
+    useState<Database["public"]["Tables"]["videos"]["Row"][]>();
   const [loading, setLoading] = useState(false);
 
   const client = createClient();
@@ -47,43 +61,51 @@ export const VideoUploader = () => {
       return;
     }
     // Create presigned url upfront
-    const { data: urlData, error: urlErr } = await client.storage
-      .from("content")
-      .createSignedUploadUrl(`video/${date.year}/${title}`);
+    // const { data: urlData, error: urlErr } = await client.storage
+    //   .from("content")
+    //   .createSignedUploadUrl(`video/${date.year}/${title}`);
 
-    if (urlErr) {
-      alert("Something went wrong while making the url - " + urlErr?.message);
-      setLoading(false);
-      return;
-    }
+    // if (urlErr) {
+    //   alert("Something went wrong while making the url - " + urlErr?.message);
+    //   setLoading(false);
+    //   return;
+    // }
     // Upload video
     // const { data: uploadData, error: uploadErr } = await client.storage
     //   .from("content")
     //   .uploadToSignedUrl(urlData?.path, urlData?.token, video!, {
     //     duplex: "half",
     //   });
-    const { data: uploadData, error: uploadErr } = await uploadFile(
-      "content",
-      urlData?.path,
-      video!,
-      client
-    );
+    // const { data: uploadData, error: uploadErr } = await uploadFile(
+    //   "content",
+    //   urlData?.path,
+    //   video!,
+    //   client
+    // );
 
     // Insert entry into video table
     // TODO make a trigger and function for this, needs to be scopped down to content table though
-    if (uploadErr) {
-      alert("Error during upload - " + uploadErr.message);
-      setLoading(false);
-      return;
-    }
+    // if (uploadErr) {
+    //   alert("Error during upload - " + uploadErr.message);
+    //   setLoading(false);
+    //   return;
+    // }
 
     // Add reference to video in db
-    const { error } = await client.from("videos").insert({
-      title,
-      date: date.toDate(getLocalTimeZone()).toDateString(),
-      path: uploadData.fullPath,
-    });
+    const { data, error } = await client
+      .from("videos")
+      .insert({
+        title,
+        date: date.toDate(getLocalTimeZone()).toDateString(),
+        path: video,
+      })
+      .select()
+      .single();
+    console.log(data);
 
+    if (data) {
+      setVidList(vidList?.concat(data));
+    }
     if (error) {
       alert(
         "Something went wrong while we were saving a reference to the video - " +
@@ -128,13 +150,13 @@ export const VideoUploader = () => {
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="video">Video</label>
+          <label htmlFor="video">Content Url</label>
           <Input
-            type="file"
-            name="Content"
+            type="text"
+            name="Content Url"
             id="content"
             onChange={(e) => {
-              setVideo(e.target.files![0]);
+              setVideo(e.target.value);
             }}
             disabled={loading}
             errorMessage={videoError}
@@ -148,6 +170,25 @@ export const VideoUploader = () => {
           Submit
         </Button>
       </div>
+
+      {/* <Table aria-label="Content Table" title="Saved Videos" className="mt-10 min-w-96">
+        <TableHeader>
+          <TableColumn>TITLE</TableColumn>
+          <TableColumn>DATE</TableColumn>
+          <TableColumn>PATH</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No rows to display."}>
+          {vidList
+            ? vidList.map((vid, idx) => (
+                <TableRow key={idx}>
+                  <TableCell>{vid.title}</TableCell>
+                  <TableCell>{vid.date}</TableCell>
+                  <TableCell>{vid.path}</TableCell>
+                </TableRow>
+              ))
+            : []}
+        </TableBody>
+      </Table> */}
     </div>
   );
 };
