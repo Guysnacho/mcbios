68b2f28743cb1fce44036e88fba4971cd1af4b6b
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-07 00:01:06 -0500

    Add role to state

--- lib/store/userStore.ts
+++ lib/store/userStore.ts
@@ -2,21 +2,29 @@ import { createStore } from "zustand";
 
 export interface UserState {
   id: string | undefined;
+  selectedRole: "student" | "postdoctorial" | "professional" | undefined;
 }
 
 export interface UserActions {
   setId: (id?: string) => void;
+  setRole: (
+    role: "student" | "postdoctorial" | "professional" | undefined
+  ) => void;
 }
 
 export type UserStore = UserState & UserActions;
 
 export const defaultInitState: UserState = {
   id: undefined,
+  selectedRole: undefined,
 };
 
 export const createUserStore = (initState: UserState = defaultInitState) => {
   return createStore<UserStore>()((set) => ({
     ...initState,
     setId: (id?: string) => set(() => ({ id })),
+    setRole: (
+      role?: "student" | "postdoctorial" | "professional" | undefined
+    ) => set(() => ({ selectedRole: role })),
   }));
 };
