252f31c0b4a2a8a1a6f7b24159b50edbdf67d3e6
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-07 00:00:12 -0500

    Fix access token url

--- lib/utils/paypal.ts
+++ lib/utils/paypal.ts
@@ -3,11 +3,8 @@ export const generateAccessToken = async () => {
     const auth = Buffer.from(getPaypalId() + ":" + getPaypalSecret()).toString(
       "base64"
     );
-    const url =
-      process.env.NODE_ENV === "development"
-        ? "https://api-m.sandbox.paypal.com/v1/oauth2/token"
-        : "https://api-m.paypal.com/v1/oauth2/token";
-    const response = await fetch(`${url}/v1/oauth2/token`, {
+
+    const response = await fetch(`${getBaseUrl()}/v1/oauth2/token`, {
       method: "POST",
       body: "grant_type=client_credentials",
       headers: {
