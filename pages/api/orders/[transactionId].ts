aee181f643ce4d0a7810a33bed08cf78e95bca1e
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-06 22:33:51 -0500

    Add transaction capture route
    need to replace the route with 'dev'/'prod' endpoints

---
+++ pages/api/orders/[transactionId].ts
@@ -0,0 +1,34 @@
+import { generateAccessToken } from "@/lib/utils/paypal";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${req.query.transactionId}`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
+
+  try {
+    res.status(200).json(await response.json());
+  } catch (err) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
