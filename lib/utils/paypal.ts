a7690b3024858936319da08c46012ccfde9a0fb9
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-06 23:07:55 -0500

    Add secret fetch and use sandbox vs prod url for token

--- lib/utils/paypal.ts
+++ lib/utils/paypal.ts
@@ -1,9 +1,13 @@
 export const generateAccessToken = async () => {
   try {
-    const auth = Buffer.from(
-      getPaypalId() + ":" + process.env.NEXT_PRIVATE_PAYPAL_CLIENT_SECRET
-    ).toString("base64");
-    const response = await fetch(`${base}/v1/oauth2/token`, {
+    const auth = Buffer.from(getPaypalId() + ":" + getPaypalSecret()).toString(
+      "base64"
+    );
+    const url =
+      process.env.NODE_ENV === "development"
+        ? "https://api-m.sandbox.paypal.com/v1/oauth2/token"
+        : "https://api-m.paypal.com/v1/oauth2/token";
+    const response = await fetch(`${url}/v1/oauth2/token`, {
       method: "POST",
       body: "grant_type=client_credentials",
       headers: {
@@ -22,4 +26,10 @@ export const getPaypalId = () => {
   if (process.env.NODE_ENV === "development")
     return process.env.NEXT_PUBLIC_SANDBOX_PAYPAL_ID!;
   return process.env.NEXT_PUBLIC_PAYPAL_ID!;
+};
+
+export const getPaypalSecret = () => {
+  if (process.env.NODE_ENV === "development")
+    return process.env.NEXT_PRIVATE_SANDBOX_SECRET!;
+  return process.env.NEXT_PRIVATE_SECRET!;
 };
