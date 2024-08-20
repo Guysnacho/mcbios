9e4c6f719758f76ca8b9d10ad8a22b56dd54cfa6
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-20 17:55:00 -0500

    Revert "Add Chakras"
    This reverts commit db59ec4c078648ebf9dfcbc43ed9ec86eb12b765.

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -2,14 +2,11 @@ import * as React from "react";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { UserStoreProvider } from "./UserStateProvider";
-import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UserStoreProvider>
-      <NextUIProvider>
-        <ChakraProvider>{children}</ChakraProvider>
-      </NextUIProvider>
+      <NextUIProvider>{children}</NextUIProvider>
     </UserStoreProvider>
   );
 }
