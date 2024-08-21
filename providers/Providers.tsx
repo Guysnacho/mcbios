5d823d01286b7489d250acee9ca919355e0cce81
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-20 21:47:43 -0500

    Drop next ui provider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 
-import { NextUIProvider } from "@nextui-org/react";
 import { UserStoreProvider } from "./UserStateProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UserStoreProvider>
-      <NextUIProvider>{children}</NextUIProvider>
+      {children}
     </UserStoreProvider>
   );
 }
