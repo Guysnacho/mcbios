443f6ba0cb80e6cdacea12232e60d524955f3353
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-21 09:29:42 -0500

    Add chakra provider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 
 import { UserStoreProvider } from "./UserStateProvider";
 import { NextUIProvider } from "@nextui-org/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserStoreProvider>
-      <NextUIProvider>{children}</NextUIProvider>
-    </UserStoreProvider>
+    <ChakraProvider>
+      <UserStoreProvider>
+        <NextUIProvider>{children}</NextUIProvider>
+      </UserStoreProvider>
+    </ChakraProvider>
   );
 }
