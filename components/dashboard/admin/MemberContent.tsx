5123688b4d9edab95728c2de78ac1c8cccebf607
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-23 04:56:54 -0500

    Isolate content to component

---
+++ components/dashboard/admin/MemberContent.tsx
@@ -0,0 +1,61 @@
+import { Database } from "@/lib/utils/supabase/types";
+import { Card, CardBody, CardHeader } from "@chakra-ui/react";
+
+import "react-datepicker/dist/react-datepicker.css";
+import VideoUploader from "./VideoUploader";
+
+export const MemberContent = ({
+  videos,
+}: {
+  videos?: Database["public"]["Tables"]["videos"]["Row"][];
+}) => {
+  return (
+    <>
+      <div className="my-5">
+        <h5 className="text-center">
+          You can watch past conference recordings below
+        </h5>
+        {/* <Select
+                label="Select a year"
+                // label={currYear !== "" ? currYear : "Select a year"}
+                className="max-w-xs flex"
+                variant="bordered"
+                selectedKeys={[currYear]}
+                onChange={handleSelectionChange}
+              >
+                {ConfYears.map((conf) => (
+                  <SelectItem key={`${conf.year}`}>{`${conf.year}`}</SelectItem>
+                ))}
+              </Select> */}
+      </div>
+      <div className="container flex flex-row flex-wrap gap-5 mx-auto justify-center">
+        {videos
+          ? videos.map((vid) => (
+              <Card key={vid.id} className="max-w-md">
+                <CardHeader className="block text-center">
+                  <h6>{vid.title ?? undefined}</h6>
+                  <p>
+                    {/* {`${formatter.format(new Date(vid.date))} ` ||
+                              undefined} */}
+                    {vid.date.substring(0, vid.date.indexOf("T"))}
+                  </p>
+                </CardHeader>
+                <CardBody>
+                  <iframe
+                    className="mx-auto"
+                    src={vid.path}
+                    width={300}
+                    allow="autoplay"
+                    loading="lazy"
+                    allowFullScreen
+                  ></iframe>
+                </CardBody>
+              </Card>
+            ))
+          : undefined}
+      </div>
+    </>
+  );
+};
+
+export default VideoUploader;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,19 +1,12 @@
+import { MemberContent } from "@/components/dashboard/admin/MemberContent";
 import UserConfirm from "@/components/dashboard/admin/UserConfirm";
 import VideoUploader from "@/components/dashboard/admin/VideoUploader";
 import { createClient as createCompoentClient } from "@/lib/utils/supabase/component";
 import { createClient } from "@/lib/utils/supabase/server-props";
 import { Database } from "@/lib/utils/supabase/types";
 import { useUserStore } from "@/providers/UserStateProvider";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Divider,
-  Tab,
-  Tabs,
-  User,
-} from "@nextui-org/react";
+import { Button, Divider } from "@chakra-ui/react";
+import { Tab, Tabs, User } from "@nextui-org/react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Script from "next/script";
@@ -87,49 +80,7 @@ export default function Dashboard(props: {
           {props.user.dues_paid_at ? (
             <Tab title="Conference Content">
               <Divider className="my-5" />
-              <div className="my-5">
-                <h5 className="text-center">
-                  You can watch past conference recordings below
-                </h5>
-                {/* <Select
-                label="Select a year"
-                // label={currYear !== "" ? currYear : "Select a year"}
-                className="max-w-xs flex"
-                variant="bordered"
-                selectedKeys={[currYear]}
-                onChange={handleSelectionChange}
-              >
-                {ConfYears.map((conf) => (
-                  <SelectItem key={`${conf.year}`}>{`${conf.year}`}</SelectItem>
-                ))}
-              </Select> */}
-              </div>
-              <div className="container flex flex-row flex-wrap gap-5 mx-auto justify-center">
-                {props.videos
-                  ? props.videos.map((vid) => (
-                      <Card key={vid.id} className="max-w-md">
-                        <CardHeader className="block text-center">
-                          <h6>{vid.title ?? undefined}</h6>
-                          <p>
-                            {/* {`${formatter.format(new Date(vid.date))} ` ||
-                              undefined} */}
-                            {vid.date.substring(0, vid.date.indexOf("T"))}
-                          </p>
-                        </CardHeader>
-                        <CardBody>
-                          <iframe
-                            className="mx-auto"
-                            src={vid.path}
-                            width={300}
-                            allow="autoplay"
-                            loading="lazy"
-                            allowFullScreen
-                          ></iframe>
-                        </CardBody>
-                      </Card>
-                    ))
-                  : undefined}
-              </div>
+              <MemberContent videos={props.videos} />
             </Tab>
           ) : undefined}
           {props.user?.role === "admin" ? (
@@ -179,7 +130,7 @@ export default function Dashboard(props: {
                       confirm and grant access to everything MCBIOS!
                     </p>
                     <Button
-                      color="success"
+                      colorScheme="green"
                       className="my-5"
                       onClick={() => {
                         client
