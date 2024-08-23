c8efe5553581ff283c2d21ddc3665db41ad566a1
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-23 05:12:41 -0500

    Migrate dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,8 +5,19 @@ import { createClient as createCompoentClient } from "@/lib/utils/supabase/compo
 import { createClient } from "@/lib/utils/supabase/server-props";
 import { Database } from "@/lib/utils/supabase/types";
 import { useUserStore } from "@/providers/UserStateProvider";
-import { Button, Divider } from "@chakra-ui/react";
-import { Tab, Tabs, User } from "@nextui-org/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Script from "next/script";
@@ -72,108 +83,116 @@ export default function Dashboard(props: {
           <h3 className="text-center my-auto">Welcome</h3>
         </div>
         <Tabs
-          aria-label="Dashboard tabs"
-          color="secondary"
-          items={tabs}
-          className="flex justify-center"
+          aria-label="Dashboard Tabs"
+          size="lg"
+          // isFitted
+          variant="enclosed"
         >
-          {props.user.dues_paid_at ? (
-            <Tab title="Conference Content">
-              <Divider className="my-5" />
-              <MemberContent videos={props.videos} />
-            </Tab>
-          ) : undefined}
-          {props.user?.role === "admin" ? (
-            <Tab title="Admin">
-              <Divider className="my-5" />
+          <TabList>
+            <Tab title="Conference Content">Conference Content</Tab>
+            <Tab title="Admin">Admin</Tab>
+            <Tab title="Profile">Profile</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {props.user.dues_paid_at ? (
+                <MemberContent videos={props.videos} />
+              ) : undefined}
+            </TabPanel>
+
+            {props.user?.role === "admin" ? (
+              <TabPanel>
+                <div className="my-5 flex gap-3 mx-auto justify-center">
+                  <VideoUploader />
+                </div>
+                <Divider className="my-5" />
+                <div className="max-w-[500]px my-5 flex gap-3 mx-auto justify-center">
+                  <UserConfirm client={client} />
+                </div>
+              </TabPanel>
+            ) : undefined}
+
+            <TabPanel>
               <div className="my-5 flex gap-3 mx-auto justify-center">
-                <VideoUploader />
+                <div>
+                  <h5 className="text-center">Your Member Info</h5>
+                  <Flex>
+                    <Avatar src="https://api.dicebear.com/9.x/thumbs/png?seed=Lily&size=75" />
+                    <Box ml="3">
+                      <Text fontWeight="bold">
+                        {props.user?.fname} {props.user?.lname}
+                      </Text>
+                      <Text fontSize="sm">
+                        {props.user?.role === "admin"
+                          ? "MCBIOS Admin"
+                          : "MCBIOS Member"}
+                      </Text>
+                    </Box>
+                  </Flex>
+                </div>
               </div>
-              <Divider className="my-5" />
-              <div className="max-w-[500]px my-5 flex gap-3 mx-auto justify-center">
-                <UserConfirm client={client} />
-              </div>
-            </Tab>
-          ) : undefined}
-          <Tab title="Profile">
-            <Divider className="my-5" />
-            <div className="my-5 flex gap-3 mx-auto justify-center">
-              <div>
-                <h5 className="text-center">Your Member Info</h5>
-                <User
-                  name={`${props.user?.fname} ${props.user?.lname}`}
-                  about="current user"
-                  description={
-                    props.user?.role === "admin"
-                      ? "MCBIOS Admin"
-                      : "MCBIOS Member"
-                  }
-                  avatarProps={{
-                    src: "https://api.dicebear.com/9.x/thumbs/png?seed=Lily&size=75",
-                  }}
-                />
-              </div>
-            </div>
-            <Divider />
-            <div className="container text-center space-y-4">
-              {!props.user.dues_paid_at ? (
-                <>
-                  <h4>Welcome to MCBIOS!</h4>
-                  <p>
-                    If you haven&apos;t already, please pay your dues to finish
-                    MCBIOS onboarding and to gain access to past conference
-                    recordings, upcomming elections, and more coming soon!
-                  </p>
-                  <div className="container">
+              <Divider />
+              <div className="container text-center space-y-4">
+                {!props.user.dues_paid_at ? (
+                  <>
+                    <h4>Welcome to MCBIOS!</h4>
                     <p>
-                      If you have paid your dues, notify us here so we can
-                      confirm and grant access to everything MCBIOS!
+                      If you haven&apos;t already, please pay your dues to
+                      finish MCBIOS onboarding and to gain access to past
+                      conference recordings, upcomming elections, and more
+                      coming soon!
                     </p>
-                    <Button
-                      colorScheme="green"
-                      className="my-5"
-                      onClick={() => {
-                        client
-                          .from("confirm_request")
-                          .insert({ user_id: store.id })
-                          .then(({ error }) => {
-                            if (error) {
-                              alert(
-                                "Something went wrong while we submitting your membership request - " +
-                                  error.message
-                              );
-                              console.error(error);
-                            } else {
-                              alert(
-                                "Thank you for letting us know, we'll confirm your membership status ASAP!"
-                              );
-                            }
-                          });
-                      }}
-                    >
-                      My dues are paid
-                    </Button>
-                  </div>
-                  <div className="flex justify-center">
-                    <Script
-                      src="https://www.paypal.com/sdk/js?client-id=BAAaWKKJH9d9_1A9lYbo-zc52pLBBTCR9boQNSGOQk7OR76lLHGsUvjZDTAm4ONcsLFqflVbaKH-ylGe-0&components=hosted-buttons&enable-funding=venmo&currency=USD"
-                      onReady={() => {
-                        // @ts-expect-error Baaaaahhhhh issokay
-                        paypal
-                          .HostedButtons({
-                            hostedButtonId: "VEMTS2QGYVFQ8",
-                          })
-                          .render("#paypal-container-VEMTS2QGYVFQ8");
-                      }}
-                    ></Script>
-                    <div id="paypal-container-VEMTS2QGYVFQ8"></div>
-                  </div>
-                </>
-              ) : (
-                <></>
-              )}
-            </div>
-          </Tab>
+                    <div className="container">
+                      <p>
+                        If you have paid your dues, notify us here so we can
+                        confirm and grant access to everything MCBIOS!
+                      </p>
+                      <Button
+                        colorScheme="green"
+                        className="my-5"
+                        onClick={() => {
+                          client
+                            .from("confirm_request")
+                            .insert({ user_id: store.id })
+                            .then(({ error }) => {
+                              if (error) {
+                                alert(
+                                  "Something went wrong while we submitting your membership request - " +
+                                    error.message
+                                );
+                                console.error(error);
+                              } else {
+                                alert(
+                                  "Thank you for letting us know, we'll confirm your membership status ASAP!"
+                                );
+                              }
+                            });
+                        }}
+                      >
+                        My dues are paid
+                      </Button>
+                    </div>
+                    <div className="flex justify-center">
+                      <Script
+                        src="https://www.paypal.com/sdk/js?client-id=BAAaWKKJH9d9_1A9lYbo-zc52pLBBTCR9boQNSGOQk7OR76lLHGsUvjZDTAm4ONcsLFqflVbaKH-ylGe-0&components=hosted-buttons&enable-funding=venmo&currency=USD"
+                        onReady={() => {
+                          // @ts-expect-error Baaaaahhhhh issokay
+                          paypal
+                            .HostedButtons({
+                              hostedButtonId: "VEMTS2QGYVFQ8",
+                            })
+                            .render("#paypal-container-VEMTS2QGYVFQ8");
+                        }}
+                      ></Script>
+                      <div id="paypal-container-VEMTS2QGYVFQ8"></div>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </TabPanel>
+          </TabPanels>
         </Tabs>
       </div>
     </>
