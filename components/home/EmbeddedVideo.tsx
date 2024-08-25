d35bbe5b13a0909f587ce486272986141aba57dc
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-24 20:36:27 -0500

    Add election videos

---
+++ components/home/EmbeddedVideo.tsx
@@ -0,0 +1,20 @@
+import { Card, CardBody, CardProps } from "@chakra-ui/react";
+
+export default function EmbeddedVideo(
+  props: { src: string; className?: string | undefined } & CardProps
+) {
+  return (
+    <Card {...props}>
+      <CardBody>
+        <iframe
+          className="mx-auto"
+          src={props.src}
+          width={300}
+          allow="autoplay"
+          loading="lazy"
+          allowFullScreen
+        ></iframe>
+      </CardBody>
+    </Card>
+  );
+}
