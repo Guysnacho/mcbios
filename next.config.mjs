2ce49cf88c528f10f225680d061e275e23d5e605
Guysnacho <guysnacho2@gmail.com>
2024-10-20 00:03:44 -0500

    Add placeholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "mcbios.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
