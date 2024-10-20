56e3c9fa10b728aab05dfc56ddc3d7eee9fab4f5
Guysnacho <guysnacho2@gmail.com>
2024-10-19 23:24:24 -0500

    Scaffold Next site, we're figuring out the App directory I guess.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,46 +1,19 @@
-import { join } from 'path';
-import type { Config } from 'tailwindcss';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin';
+import type { Config } from "tailwindcss";
 
-export default {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				hero: "url('src/lib/assets/2024_bg_brad-huchteman.jpg')"
-			}
-		}
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'hamlindigo',
-						enhancements: true
-					},
-					{
-						name: 'gold-nouveau',
-						enhancements: true
-					},
-					{
-						name: 'rocket',
-						enhancements: true
-					},
-					{
-						name: 'modern',
-						enhancements: true
-					}
-				]
-			}
-		})
-	]
-} satisfies Config;
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
