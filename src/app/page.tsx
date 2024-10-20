9584633ee8b05994c8a66802b1375d89fa9cd0f2
Guysnacho <guysnacho2@gmail.com>
2024-10-20 00:18:09 -0500

    Add placeholder hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Button, Flex, Stack, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,13 +15,52 @@ export default function Home() {
           height={187}
           priority
         />
-        <h6 className="mx-auto text-md text-center font-[family-name:var(--font-geist-mono)]">
-          MCBIOS 2025 is{" "}
-          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-            Under Construction
-          </code>
-          .
-        </h6>
+        <Flex
+          w={"full"}
+          h={"100vh"}
+          backgroundImage={
+            "url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)"
+          }
+          backgroundSize={"cover"}
+          backgroundPosition={"center center"}
+        >
+          <VStack
+            w={"full"}
+            justify={"center"}
+            px={[4, null, null, 8]}
+            bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+          >
+            <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+              <Text
+                color={"white"}
+                fontWeight={700}
+                lineHeight={1.2}
+                fontSize={["3xl", null, null, "4xl"]}
+              >
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                eiusmod tempor
+              </Text>
+              <Stack direction={"row"}>
+                <Button
+                  bg={"blue.400"}
+                  rounded={"full"}
+                  color={"white"}
+                  _hover={{ bg: "blue.500" }}
+                >
+                  Show me more
+                </Button>
+                <Button
+                  bg={"whiteAlpha.300"}
+                  rounded={"full"}
+                  color={"white"}
+                  _hover={{ bg: "whiteAlpha.500" }}
+                >
+                  Show me more
+                </Button>
+              </Stack>
+            </Stack>
+          </VStack>
+        </Flex>
       </main>
     </div>
   );
