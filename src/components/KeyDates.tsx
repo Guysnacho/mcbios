184a6ccd998183cef0aecf1411ae7f894ae11b16
Samuel Adetunji <guysnacho2@gmail.com>
2024-10-21 19:30:31 -0500

    Add key dates

--- src/components/KeyDates.tsx
+++ src/components/KeyDates.tsx
@@ -6,14 +6,14 @@ import { PiChalkboardTeacher } from "react-icons/pi";
 
 interface ConfDayProps {
   title: string;
-  text: string;
+  text?: string;
   deadline?: string;
   icon: ReactElement;
 }
 
 const Date = ({ title, text, deadline, icon }: ConfDayProps) => {
   return (
-    <Stack>
+    <Stack border="2px" borderColor="gray.300" p={4} borderRadius={5}>
       <Flex
         w={16}
         h={16}
@@ -27,7 +27,7 @@ const Date = ({ title, text, deadline, icon }: ConfDayProps) => {
         {icon}
       </Flex>
       <Text fontWeight={600}>{title}</Text>
-      <Text color={"gray.600"}>{text}</Text>
+      {text && <Text color={"gray.600"}>{text}</Text>}
       {deadline && <Text color={"maroon"}>{deadline}</Text>}
     </Stack>
   );
@@ -41,7 +41,7 @@ export default function KeyDates() {
           icon={<Icon as={PiChalkboardTeacher} w={10} h={10} />}
           title={"Call for Session Proposals"}
           text={
-            "If you'd like to hold a session or conduct a workshop during the conference, the deadline is - "
+            "If you'd like to hold a session or conduct a workshop during the conference, the deadline is"
           }
           deadline="November 25th, 2024"
         />
@@ -49,7 +49,7 @@ export default function KeyDates() {
           icon={<Icon as={PiChalkboardTeacher} w={10} h={10} />}
           title={"Call for Poster Submissions"}
           text={
-            "If you have reasearch and applicable development that you want to showcase, please let the team know! The deadline for submitting a poster is -"
+            "If you have reasearch and applicable development that you want to showcase, please let the team know! The deadline for submitting a poster is"
           }
           deadline="December 15th, 2024"
         />
@@ -57,16 +57,13 @@ export default function KeyDates() {
           icon={<Icon as={PiChalkboardTeacher} w={10} h={10} />}
           title={"Young Scientist Excellence Award Application Deadline"}
           text={
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
+            "This awards program recognizes students and postdoctoral fellows that exhibit scientific excellence in the field of Bioinformatics. The deadline for this award is"
           }
           deadline="January 15th, 2024"
         />
         <Date
           icon={<Icon as={PiChalkboardTeacher} w={10} h={10} />}
           title={"Early Bird Registration"}
-          text={
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore..."
-          }
           deadline="February 1st, 2024"
         />
       </SimpleGrid>
