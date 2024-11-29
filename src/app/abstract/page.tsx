daa5ec54cd28e8edcd378af0101a9ea26e09e436
Guysnacho <guysnacho2@gmail.com>
2024-11-29 12:50:46 -0600

    Add prelim info for abstract sub

--- src/app/abstract/page.tsx
+++ src/app/abstract/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-import { Box, Flex, Stack, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import localFont from "next/font/local";
 import Head from "next/head";
+
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function Page() {
   return (
@@ -55,6 +72,62 @@ export default function Page() {
         </Flex>
         {/* Abstract Submissions */}
       </Box>
+      <SimpleGrid
+        w="full"
+        mx="auto"
+        justifyContent="space-evenly"
+        py="20"
+        bgGradient="linear(to-br, whiteAlpha.200, whiteAlpha.400, orange.100, blue.300)"
+        columns={{ base: 1, lg: 2 }}
+        spacing={10}
+      >
+        <VStack
+          w={{ base: "90%", lg: "75%" }}
+          mx="auto"
+          align="stretch"
+          spacing={5}
+        >
+          <Heading size="lg" color="blue.800">
+            Abstract Submission Instructions
+          </Heading>
+          <Text>
+            There are a limited number of spots for oral presentations. If not
+            selected for oral presentation, you will automatically be considered
+            for poster presentation. Oral presentations will generally be 15 min
+            long and the speaker would participate in a panel question and
+            answer session.
+          </Text>
+          <Text>
+            Poster size limitation: 30&quot;W x 20&quot;H (76cm x 51cm)
+          </Text>
+          <Text>The deadline to submit an abstract is February 1st, 2025</Text>
+        </VStack>
+        <Box
+          w={{ base: "90%", lg: "50%" }}
+          mx="auto"
+          justifyContent="space-around"
+        >
+          <Card
+            w={{ base: "full", md: "md" }}
+            _hover={{ shadow: "xl", borderColor: "blue.600" }}
+            borderColor="blue.400"
+            borderWidth={0.5}
+          >
+            <CardBody>
+              <Stack spacing="3">
+                <Heading
+                  size="xl"
+                  color="blue.800"
+                  textAlign="center"
+                  className={geistMono.className}
+                >
+                  MCBIOS 2024 Registration
+                </Heading>
+              </Stack>
+            </CardBody>
+          </Card>
+        </Box>
+      </SimpleGrid>
     </VStack>
   );
 }
