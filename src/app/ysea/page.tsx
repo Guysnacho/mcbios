95034704fea666973d1d4532d56b301e9c0c0acb
Samuel Adetunji <guysnacho2@gmail.com>
2025-01-19 23:19:54 -0600

    Bump YSEA deadline

--- src/app/ysea/page.tsx
+++ src/app/ysea/page.tsx
@@ -79,7 +79,7 @@ export default function Page() {
                     color={"blue.100"}
                     decoration="underline"
                   >
-                    Deadline: January 15th, 2025
+                    Deadline: January 30th, 2025
                   </Text>
                   <Heading
                     color={"white"}
