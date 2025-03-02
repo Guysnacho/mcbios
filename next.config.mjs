28853748739b21d72987130ad8d5f6be3096d04f
Guysnacho <tunjiprod@gmail.com>
2025-03-02 01:08:48 -0600

    Try to fix build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@
 const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+    // You may not need this, it's just to support moduleResolution: 'node16'
+    extensionAlias: {
+      '.js': ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    turbo: {
+      resolveAlias: {
+        // Turbopack does not support standard ESM import paths yet
+        './program.js': './app/program/page.tsx',
+        /**
+         * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
+         * Module parse failed: Unexpected character '�' (1:0)" error
+         */
+        canvas: './empty-module.ts',
+      },
+    },
   },
   images: {
     remotePatterns: [
