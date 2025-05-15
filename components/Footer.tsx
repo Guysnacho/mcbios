e7dbf22f4b69720210f6c6a2cbfcc028662f5805
Guysnacho <tunjiprod@gmail.com>
2025-05-14 22:37:17 -0500

    Fix linkedin icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -71,22 +71,20 @@ const social = [
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/company/midsouth-computational-biology-and-bioinformatics-society/",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        fill="currentColor"
-        viewBox="0 0 256 256"
-      >
-        <path d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"></path>
+    icon: (props: any) => (
+      <svg fill="currentColor" viewBox="0 0 256 256" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"
+          clipRule="evenodd"
+        ></path>
       </svg>
     ),
   },
   {
     name: "GitHub",
     href: "https://github.com/Guysnacho/mcbios",
-    icon: (props) => (
+    icon: (props: any) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
           fillRule="evenodd"
