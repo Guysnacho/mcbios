62b056935759bd8caa6c7fdca916f93b12ea0def
Guysnacho <tunjiprod@gmail.com>
2025-07-11 17:52:56 -0500

    Hid entries properlly

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,15 +1,37 @@
+"use client";
 import { NAV_ITEMS } from "@/lib/constants";
 import { Box, Heading, HStack, Link } from "@chakra-ui/react";
 
-export default function Navbar() {
+type NavbarProps = {
+  underConstruction?: boolean;
+};
+
+export default function Navbar({ underConstruction }: NavbarProps) {
   return (
-    <Box w="full" color="green" spaceY={5}>
-      <Heading size={"3xl"} textAlign="center">
+    <Box w="full" py={8} bg="#7a84df" color="green" spaceY={5}>
+      <Heading size={["xl", null, "2xl", "4xl"]} textAlign="center">
         MCBIOS 2026
       </Heading>
-      <HStack justifyContent="center" alignItems="center" gap="10">
+      <HStack
+        hidden={underConstruction}
+        display="none"
+        lg={{
+          display: "flex",
+        }}
+        justifyContent="center"
+        alignItems="center"
+        gap="10"
+      >
         {NAV_ITEMS.map((item, idx) => (
-          <Heading key={idx} colorPalette="blue" size={"md"}>
+          <Heading
+            key={idx}
+            colorPalette="blue"
+            size={"md"}
+            _hover={{
+              textDecoration: "none",
+              shadow: "lg",
+            }}
+          >
             <Link variant="underline" href={item.path}>
               {item.name}
             </Link>
