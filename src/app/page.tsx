8bd15e68d0926e82ff79a8417e6ddc87d488f7a1
Guysnacho <tunjiprod@gmail.com>
2025-07-11 17:25:38 -0500

    Add image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import { Image, Stack } from "@chakra-ui/react";
+import { Heading, Image, Stack } from "@chakra-ui/react";
 import NextImage from "next/image";
 
 export default function Home() {
   return (
     <Stack>
-      <Image asChild >
+      <Heading >Home page thiiiiiiiings</Heading>
+      <Image asChild width={500} mx="auto">
         <NextImage
           src="/img/Moffitt.jpg"
           alt="Moffit national cancer hospital."
+          width={1200}
+          height={800}
         />
       </Image>
     </Stack>
