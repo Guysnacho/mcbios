69d2c28d9bc0ba6495bc45eba38b267eb8d10759
Guysnacho <tunjiprod@gmail.com>
2025-07-11 17:28:05 -0500

    Add placeholder nav items

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -3,14 +3,12 @@ import { Box, Heading, HStack, Link } from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
-    <Box w="full" color="green">
-      <Heading size={"3xl"}>MCBIOS 2026</Heading>
+    <Box w="full" color="green" spaceY={5}>
+      <Heading size={"3xl"} textAlign="center">
+        MCBIOS 2026
+      </Heading>
       <HStack justifyContent="center" alignItems="center" gap="10">
-        {NAV_ITEMS.concat(
-          NAV_ITEMS,
-          NAV_ITEMS,
-          NAV_ITEMS
-        ).map((item, idx) => (
+        {NAV_ITEMS.map((item, idx) => (
           <Heading key={idx} colorPalette="blue" size={"md"}>
             <Link variant="underline" href={item.path}>
               {item.name}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,16 @@ export const NAV_ITEMS = [
     name: "Home",
     path: "/",
   },
+  {
+    name: "Registration",
+    path: "/",
+  },
+  {
+    name: "Submissions",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
 ];
