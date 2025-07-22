b3340ee1103c01acb89dc50af6d03b17824ce37c
Guysnacho <tunjiprod@gmail.com>
2025-07-21 20:29:56 -0500

    Better gradient on nav

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,5 +1,11 @@
 import { NAV_ITEMS } from "@/lib/constants";
-import { Box, Heading, HStack, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Link as ChakraLink,
+  Separator,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 
 type NavbarProps = {
@@ -9,56 +15,61 @@ type NavbarProps = {
 export default function Navbar({ underConstruction }: NavbarProps) {
   return (
     <Box
-      zIndex="max"
       w="full"
-      py={8}
-      bg="background.100"
-      shadow="xs"
+      py={4}
+      px={[0, null, null, 5]}
+      bgImage="linear-gradient(120deg, {colors.secondary.800}, {colors.secondary.600}, {colors.secondary.600}, {colors.accent.50})"
       color="text"
-      spaceY={5}
-      bgGradient="to-tl"
-      gradientFrom="secondary.100"
-      gradientTo="primary.200"
+      spaceY={3}
     >
       <Heading
-        size={["xl", null, "2xl", "5xl"]}
-        textAlign="center"
+        size={["xl", null, "2xl", "3xl"]}
+        textAlign={["center", null, null, "start"]}
         userSelect="none"
         backgroundClip="text"
         color="transparent"
         bgGradient="to-tl"
-        gradientFrom="primary.600"
         gradientTo="secondary.500"
-        _hover={{
-          gradientFrom: "accent.800",
-          gradientTo: "secondary.700",
-        }}
+        gradientFrom="primary.200"
+        _hover={
+          {
+            // gradientFrom: "accent.800",
+            // gradientTo: "secondary.700",
+          }
+        }
       >
         MCBIOS 2026
       </Heading>
+      <Separator colorPalette="white" ml={-3} />
       <HStack
         hidden={underConstruction}
         display="none"
         lg={{
           display: "flex",
         }}
-        justifyContent="center"
-        alignItems="center"
-        gap="10"
+        justifyContent="start"
+        alignItems="start"
+        gap={5}
       >
         {NAV_ITEMS.map((item, idx) => (
           <Heading key={idx} size={"md"}>
             <ChakraLink
               asChild
-              backgroundClip="text"
-              color="transparent"
-              bgGradient="to-tl"
-              gradientTo="primary.600"
-              gradientFrom="secondary.500"
+              // backgroundClip="text"
+              // color="transparent"
+              // bgGradient="to-tl"
+              // gradientTo="primary.600"
+              // gradientFrom="secondary.500"
               _hover={{
                 textDecoration: "underline",
-                textDecorationStyle: "solid",
-                textDecorationColor: "primary.600",
+                // color: "secondary.300",
+                backgroundClip: "text",
+                color: "transparent",
+                bgGradient: "to-tl",
+                gradientTo: "primary.600",
+                gradientFrom: "secondary.500",
+                // textDecorationStyle: "solid",
+                // textDecorationColor: "primary.600",
               }}
             >
               <NextLink href={item.path}>{item.name}</NextLink>
