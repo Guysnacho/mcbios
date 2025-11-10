e3ba7a022ed3d109f3fdc8ea369a8c1059457099
Guysnacho <tunjiprod@gmail.com>
2025-11-10 15:09:16 -0600

    update images

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com'], // Add domains for external images
-  }
+    remotePatterns: [new URL("https://images.unsplash.com/**")], // Add domains for external images
+  },
 };
 
 export default nextConfig;
