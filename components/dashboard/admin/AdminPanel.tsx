0ba54e3cfe2ab67f1627e9293a00f93ddf9426f3
Guysnacho <tunjiprod@gmail.com>
2026-01-23 16:57:57 -0600

    Park registrations panel

--- components/dashboard/admin/AdminPanel.tsx
+++ components/dashboard/admin/AdminPanel.tsx
@@ -1,17 +1,17 @@
 import { Database } from "@/lib/supabase/types";
 import {
-  TabPanel,
-  Divider,
   Heading,
-  Tabs,
-  TabList,
   Tab,
+  TabList,
+  TabPanel,
   TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { CouponCreator } from "./CouponCreator";
 import UserConfirm from "./UserConfirm";
 import VideoUploader from "./VideoUploader";
+import RawRegistrations from "./RawRegistrations";
 
 type AdminPanelProps = {
   client: SupabaseClient<Database>;
@@ -27,11 +27,28 @@ export function AdminPanel({ client }: AdminPanelProps) {
         colorScheme="purple"
       >
         <TabList>
+          <Tab>Registration Info</Tab>
           <Tab>Member Confirmation</Tab>
           <Tab>Content Update</Tab>
           <Tab>Coupon Creator</Tab>
         </TabList>
         <TabPanels>
+          <TabPanel>
+            <Heading size="md" textAlign="center">
+              Un-Authenticated Registrations
+            </Heading>
+            <div className="max-w-[500]px my-5 flex gap-3 mx-auto justify-center">
+              <RawRegistrations client={client} />
+            </div>
+
+            <Heading size="md" textAlign="center">
+              Membership
+            </Heading>
+            <div className="max-w-[500]px my-5 flex gap-3 mx-auto justify-center">
+              <UserConfirm client={client} />
+            </div>
+          </TabPanel>
+
           <TabPanel>
             <Heading size="md" textAlign="center">
               User Account Confirmation
