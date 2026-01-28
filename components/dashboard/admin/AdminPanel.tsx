c592f43a878bf9631abc70887465678213ed743a
Samuel Adetunji <tunjiprod@gmail.com>
2026-01-27 20:13:11 -0600

    Working on Registration Table (#113)
    * Park registrations panel
    
    * build fetcher
    
    * Add registration tables and fetchers

--- components/dashboard/admin/AdminPanel.tsx
+++ components/dashboard/admin/AdminPanel.tsx
@@ -1,15 +1,15 @@
 import { Database } from "@/lib/supabase/types";
 import {
-  TabPanel,
-  Divider,
   Heading,
-  Tabs,
-  TabList,
   Tab,
+  TabList,
+  TabPanel,
   TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { CouponCreator } from "./CouponCreator";
+import Registrations from "./Registrations";
 import UserConfirm from "./UserConfirm";
 import VideoUploader from "./VideoUploader";
 
@@ -27,11 +27,37 @@ export function AdminPanel({ client }: AdminPanelProps) {
         colorScheme="purple"
       >
         <TabList>
+          <Tab>Registration Info</Tab>
           <Tab>Member Confirmation</Tab>
           <Tab>Content Update</Tab>
           <Tab>Coupon Creator</Tab>
         </TabList>
         <TabPanels>
+          <TabPanel>
+            <div className="max-w-[500]px my-5 space-y-5 mx-auto justify-center">
+              <div>
+                <Heading size="md" textAlign="center">
+                  Authenticated Registrations
+                </Heading>
+                <Registrations currentMembers />
+              </div>
+
+              <div>
+                <Heading size="md" textAlign="center">
+                  Un-Authenticated Registrations
+                </Heading>
+                <Registrations />
+              </div>
+            </div>
+
+            <Heading size="md" textAlign="center">
+              Membership
+            </Heading>
+            <div className="max-w-[500]px my-5 flex gap-3 mx-auto justify-center">
+              <UserConfirm client={client} />
+            </div>
+          </TabPanel>
+
           <TabPanel>
             <Heading size="md" textAlign="center">
               User Account Confirmation
