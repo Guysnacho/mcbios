14550f4752a9adb58e99d25fa4be0049ec38ff27
Guysnacho <tunjiprod@gmail.com>
2026-01-27 19:21:50 -0600

    build fetcher

--- lib/swrFetchers.ts
+++ lib/swrFetchers.ts
@@ -1,6 +1,6 @@
 import { SupabaseClient } from "@supabase/supabase-js";
-import { Database } from "./supabase/types";
 import Stripe from "stripe";
+import { Database } from "./supabase/types";
 
 export const authFetcher = async (client: SupabaseClient<Database>) => {
   // Fetch data from external API
@@ -25,6 +25,37 @@ export const couponFetcher = async (): Promise<CouponList> => {
   return await data.json();
 };
 
+export const memberFetcher = async (client: SupabaseClient<Database>) => {
+  const rawRegistrationPromise = client
+    .from("raw_registration")
+    .select(`fname, lname, role, email, institution`)
+    .eq("org_id", process.env.NEXT_PUBLIC_ORG_ID)
+    .throwOnError()
+    .then((res) => {
+      res.data;
+    });
+
+  const memberPromise = client
+    .from("member")
+    .select(`*`)
+    .eq("org_id", process.env.NEXT_PUBLIC_ORG_ID)
+    .throwOnError()
+    .then((res) => {
+      res.data;
+    });
+
+  const [registrations, memberRes] = await Promise.all([
+    rawRegistrationPromise,
+    memberPromise,
+  ]);
+  return {
+    // @ts-expect-error whoopsie
+    registrations: registrations as Registration[],
+    // @ts-expect-error whoopsie
+    members: memberRes as Database["public"]["Tables"]["member"]["Row"][],
+  };
+};
+
 type CouponList = {
   coupon: Stripe.Coupon;
   active: boolean;
@@ -34,4 +65,14 @@ type CouponList = {
   max_redemptions: number | null;
   times_redeemed: number;
   expires_at: number | null;
-}[]
+}[];
+
+export type Registration =
+  | {
+      fname: string;
+      lname: string;
+      role: "professional" | "student" | "admin" | "postdoctorial";
+      email: string;
+      institution: string;
+    }
+  | Database["public"]["Tables"]["member"]["Row"];
