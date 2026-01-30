b01e896bbc4e071cda89f8820b900c1d54c0c26e
Guysnacho <tunjiprod@gmail.com>
2026-01-29 20:18:10 -0600

    meh

--- app/leadership/layout.tsx
+++ app/leadership/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     "MCBIOS is a non-profit organization founded in 2003. Here you can find the board that makes this society operate.",
 };
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function Layout(props: { children: React.ReactNode }) {
   const { children } = props;
   return <>{children}</>;
 }
--- app/membership/layout.tsx
+++ app/membership/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     "MCBIOS is a non-profit organization founded in 2003. Here you can learn about the separate membership tiers along with other registration details.",
 };
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default function Layout(props: { children: React.ReactNode }) {
   const { children } = props;
   return <>{children}</>;
 }
