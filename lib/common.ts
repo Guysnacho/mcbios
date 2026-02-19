de299477ebc226f912d3499ffc4f376fa0f87af6
Guysnacho <tunjiprod@gmail.com>
2026-02-18 18:19:46 -0600

    Add enriched analytics hook

--- lib/common.ts
+++ lib/common.ts
@@ -1,5 +1,25 @@
+import { useAptabase } from "@aptabase/react";
+
 export const isPresent = (str?: string) => {
   return str !== undefined && str !== "";
 };
 
-export const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const wait = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export const useAnalytics = () => {
+  const { trackEvent: aptabaseTrackEvent } = useAptabase();
+
+  function trackEvent(
+    eventName: string,
+    eventBody: Record<string, string | number | boolean> | undefined,
+  ) {
+    aptabaseTrackEvent(eventName, {
+      ...eventBody,
+      org: process.env.NEXT_PUBLIC_ORG_ID,
+      url: process.env.NEXT_PUBLIC_APP_URL,
+    });
+  }
+
+  return { trackEvent };
+};
