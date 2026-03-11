174be96557d5352f42e2496ea94c3eeae70a9f8a
Guysnacho <tunjiprod@gmail.com>
2026-03-11 09:11:23 -0500

    allow cors headers?

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@ const { version } = require('./package.json')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply CORS headers to all API routes
+        source: "/checkout",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "localhost, mcbios", // Allow all origins (or specify specific domains)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, session_id",
+          }
+        ],
+      },
+    ];
+  },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
